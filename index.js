779a5e171915d9c516cef3fa9bb40f8a8117a069
unknown <anshu7kool@gmail.com>
2018-09-04 20:39:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+	res.sendFile(__dirname+'/index.html');
+
+});
+app.get('/hello',function(req,res){
+	res.sendFile(__dirname+'/new 1.html');
+});
+var port = process.env.port || 8080;
+var server = app.listen(port,function(req,res){
+	console.log("catch the action at http://localhost:"+port);
+});
