2a5c71eb4533f803feb319a75a2ce26036aa5e9d
unknown <anshu7kool@gmail.com>
2018-09-04 21:20:46 +0530

    added script

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/',function(req,res){
 app.get('/hello',function(req,res){
 	res.sendFile(__dirname+'/new 1.html');
 });
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 var server = app.listen(port,function(req,res){
 	console.log("catch the action at http://localhost:"+port);
 });
